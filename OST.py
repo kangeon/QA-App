9ab7ee2afe126d10215c22c09a7d33f3320c8ce4
Geon Kang <gk698@nyu.edu>
2014-12-10 18:30:55 -0500

    Implemented adding new questions. (tag feature not yet implemented)

--- OST.py
+++ OST.py
@@ -12,6 +12,20 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
 	extensions=['jinja2.ext.autoescape'],
 	autoescape=True)
+
+DEFAULT_QUESTIONLIST_NAME = 'default_questionlist'
+
+def questionlist_key(questionlist_name=DEFAULT_QUESTIONLIST_NAME):
+    return ndb.Key('Questionlist', questionlist_name)
+
+class Question(ndb.Model):
+    """Models an individual question."""
+    author = ndb.UserProperty()
+    title = ndb.StringProperty(indexed=False)
+    content = ndb.TextProperty()
+    creationtime = ndb.DateTimeProperty(auto_now_add=True)
+    modifiedtime = ndb.DateTimeProperty(auto_now=True)
+    tags = ndb.StringProperty(repeated=True)
 	
 def header(self):
     if users.get_current_user():
@@ -35,7 +49,51 @@ def header(self):
 class MainPage(webapp2.RequestHandler):
     def get(self):
       header(self)
+      questions_query = Question.query(ancestor=questionlist_key(DEFAULT_QUESTIONLIST_NAME)).order(-Question.creationtime)
+      questions = questions_query.fetch(10)
+
+      main_values = {
+        'questions' : questions,
+      }
+      
+      main_template = JINJA_ENVIRONMENT.get_template('main.html')
+      self.response.write(main_template.render(main_values))
+
+    def post(self):
+      header(self)
+      question = Question(parent=questionlist_key(DEFAULT_QUESTIONLIST_NAME))
+      question.author = users.get_current_user()
+      question.title = cgi.escape(self.request.get('qtitle'))
+      question.content = cgi.escape(self.request.get('qcontent'))
+      #question.tags = cgi.escape(self.request.get('qtags'))
+      question.put()
+
+      questions_query = Question.query(ancestor=questionlist_key(DEFAULT_QUESTIONLIST_NAME)).order(-Question.creationtime)
+      questions = questions_query.fetch(10)
+
+      main_values = {
+        'questions' : questions,
+      }
+      
+      main_template = JINJA_ENVIRONMENT.get_template('main.html')
+      self.response.write(main_template.render(main_values))
+
+
+
+class Create(webapp2.RequestHandler):
+    def get(self):
+      header(self)
+      if users.get_current_user():
+        user_logged_on = 1
+      else:
+        user_logged_on = 0
+      create_values = {
+        'user_logged_on' : user_logged_on,
+      }
+      create_template = JINJA_ENVIRONMENT.get_template('create.html')
+      self.response.write(create_template.render(create_values))
 
 application = webapp2.WSGIApplication([
     ('/', MainPage),
+    ('/create', Create),
 ], debug=True)
