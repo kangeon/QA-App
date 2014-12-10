e1c6ef8fdb6d8422d3a926f9de64155c0ad1f36e
Geon Kang <gk698@nyu.edu>
2014-12-10 15:54:06 -0500

    Added login feature and header.html rendered with jinja2

--- OST.py
+++ OST.py
@@ -1,29 +1,41 @@
 import cgi
+import os
+import urllib
+
 from google.appengine.api import users
+from google.appengine.ext import ndb
+
+import jinja2
 import webapp2
 
-MAIN_PAGE_HTML = """\
-<html>
-  <body>
-    <form action="/sign" method="post">
-      <div><textarea name="content" rows="3" cols="60"></textarea></div>
-      <div><input type="submit" value="Sign Guestbook"></div>
-    </form>
-  </body>
-</html>
-"""
-
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+	extensions=['jinja2.ext.autoescape'],
+	autoescape=True)
+	
+def header(self):
+    if users.get_current_user():
+	   user_logged_on = 1
+	   log_url = users.create_logout_url(self.request.uri)
+	   log_url_linktext = 'Logout'
+    else:
+	   user_logged_on = 0
+	   log_url = users.create_login_url(self.request.uri)
+	   log_url_linktext = 'Login'
+	
+    header_values = {
+        'user_logged_on' : user_logged_on,
+        'log_url' : log_url,
+        'log_url_linktext' : log_url_linktext,
+    }
+	
+    header_template = JINJA_ENVIRONMENT.get_template('header.html')
+    self.response.write(header_template.render(header_values))
+		
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        self.response.write(MAIN_PAGE_HTML)
-
-class Guestbook(webapp2.RequestHandler):
-    def post(self):
-        self.response.write('<html><body>You wrote:<pre>')
-        self.response.write(cgi.escape(self.request.get('content')))
-        self.response.write('</pre></body></html>')
+      header(self)
 
 application = webapp2.WSGIApplication([
     ('/', MainPage),
-    ('/sign', Guestbook),
 ], debug=True)
